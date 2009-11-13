e67281991f9d555d0540720d006500d62f900e0b
Marcel Molina <marcel@twitter.com>
2009-11-13 14:42:57 -0800

    Finish initial implementation of aliases controller with tests.

--- lib/twurl/aliases_controller.rb
+++ lib/twurl/aliases_controller.rb
@@ -1,14 +1,24 @@
 module Twurl
   class AliasesController < AbstractCommandController
+    NO_ALIASES_MESSAGE       = "No aliases exist. Set one this way: twurl alias h /1/statuses/home_timeline.xml"
+    NO_PATH_PROVIDED_MESSAGE = "No path was provided to alias. Paths must start with a forward slash (ex. /1/statuses/update.xml)."
     def dispatch
       case options.subcommands.size
       when 0
         aliases = OAuthClient.rcfile.aliases
-        aliases.keys.sort.each do |name|
-          CLI.puts "#{name}: #{aliases[name]}"
+        if aliases && !aliases.empty?
+          aliases.keys.sort.each do |name|
+            CLI.puts "#{name}: #{aliases[name]}"
+          end
+        else
+          CLI.puts NO_ALIASES_MESSAGE
         end
       when 1
-        OAuthClient.rcfile.alias(options.subcommands.first, options.path)
+        if options.path
+          OAuthClient.rcfile.alias(options.subcommands.first, options.path)
+        else
+          CLI.puts NO_PATH_PROVIDED_MESSAGE
+        end
       end
     end
   end
---
+++ test/alias_controller_test.rb
@@ -0,0 +1,53 @@
+require File.dirname(__FILE__) + '/test_helper'
+
+class Twurl::AliasesController::DispatchTest < Test::Unit::TestCase
+  attr_reader :options, :client
+  def setup
+    @options = Twurl::CLI::Options.test_exemplar
+    @client  = Twurl::OAuthClient.test_exemplar
+
+    # Clean slate
+    if Twurl::OAuthClient.rcfile.aliases
+      Twurl::OAuthClient.rcfile.aliases.clear
+    end
+
+    stub(Twurl::OAuthClient.rcfile).save
+  end
+
+  def test_when_no_subcommands_are_provided_and_no_aliases_exist_nothing_is_displayed
+    assert options.subcommands.empty?
+    mock(Twurl::CLI).puts(Twurl::AliasesController::NO_ALIASES_MESSAGE).times(1)
+
+    controller = Twurl::AliasesController.new(client, options)
+    controller.dispatch
+  end
+
+  def test_when_no_subcommands_are_provided_and_aliases_exist_they_are_displayed
+    assert options.subcommands.empty?
+
+    Twurl::OAuthClient.rcfile.alias('h', '/1/statuses/home_timeline.xml')
+    mock(Twurl::CLI).puts("h: /1/statuses/home_timeline.xml").times(1)
+
+    controller = Twurl::AliasesController.new(client, options)
+    controller.dispatch
+  end
+
+  def test_when_alias_and_value_are_provided_they_are_added
+    options.subcommands = ['h']
+    options.path        = '/1/statuses/home_timeline.xml'
+    mock(Twurl::OAuthClient.rcfile).alias('h', '/1/statuses/home_timeline.xml').times(1)
+
+    controller = Twurl::AliasesController.new(client, options)
+    controller.dispatch
+  end
+
+  def test_when_no_path_is_provided_nothing_happens
+    options.subcommands = ['a']
+    assert_nil options.path
+
+    mock(Twurl::CLI).puts(Twurl::AliasesController::NO_PATH_PROVIDED_MESSAGE).times(1)
+
+    controller = Twurl::AliasesController.new(client, options)
+    controller.dispatch
+  end
+end
