4f38cd6bcd7294db7b6721ca86d2d72b5301afc6
Marcel Molina <marcel@twitter.com>
2010-09-29 22:32:26 -0700

    Bump version for release.

--- lib/twurl/version.rb
+++ lib/twurl/version.rb
@@ -2,7 +2,7 @@ module Twurl
   module VERSION
     MAJOR  = '0' unless defined? MAJOR
     MINOR  = '6' unless defined? MINOR
-    TINY   = '1' unless defined? TINY
+    TINY   = '2' unless defined? TINY
     BETA   = nil unless defined? BETA # Time.now.to_i.to_s
   end
 
