bc4a94f68f11ec541f6eb9b9a5d4f3926e06eaef
Erik Michaels-Ober <sferik@gmail.com>
2012-04-27 19:17:25 -0700

    Bump version for release

--- lib/twurl/version.rb
+++ lib/twurl/version.rb
@@ -2,9 +2,9 @@ module Twurl
   module VERSION
     MAJOR  = '0' unless defined? MAJOR
     MINOR  = '6' unless defined? MINOR
-    TINY   = '6' unless defined? TINY
+    PATCH  = '7' unless defined? PATCH
     BETA   = nil unless defined? BETA # Time.now.to_i.to_s
   end
 
-  Version = [VERSION::MAJOR, VERSION::MINOR, VERSION::TINY, VERSION::BETA].compact * '.'
+  Version = [VERSION::MAJOR, VERSION::MINOR, VERSION::PATCH, VERSION::BETA].compact * '.'
 end
