b0123c1c3f717e7446b885b39413221da173b706
Jacob Petrie <jpetrie@twitter.com>
2014-03-24 18:50:51 -0700

    update help text, fix for issue 41

--- lib/twurl/cli.rb
+++ lib/twurl/cli.rb
@@ -304,7 +304,7 @@ Supported Commands: #{SUPPORTED_COMMANDS.sort.join(', ')}
       end
       
       def filefield 
-        on('-F', '--file-field [field_name]', 'Specify the file field for the upload to be included within (default: media)') do |filefield|
+        on('-F', '--file-field [field_name]', 'Specify the POST parameter name for the file upload data (default: media)') do |filefield|
           options.upload['filefield'] = filefield
         end
       end
