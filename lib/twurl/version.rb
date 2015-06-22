6bec9960b4f6cba6dcc9ea53772b459a9356db41
Erik Michaels-Ober <sferik@gmail.com>
2015-06-22 14:28:36 +0200

    Bump version to 0.9.3

--- lib/twurl/version.rb
+++ lib/twurl/version.rb
@@ -2,13 +2,13 @@ module Twurl
   class Version
     MAJOR = 0 unless defined? Twurl::Version::MAJOR
     MINOR = 9 unless defined? Twurl::Version::MINOR
-    PATCH = 2 unless defined? Twurl::Version::PATCH
-    BETA = nil unless defined? Twurl::Version::BETA # Time.now.to_i.to_s
+    PATCH = 3 unless defined? Twurl::Version::PATCH
+    PRE = nil unless defined? Twurl::Version::PRE # Time.now.to_i.to_s
 
     class << self
       # @return [String]
       def to_s
-        [MAJOR, MINOR, PATCH, BETA].compact.join('.')
+        [MAJOR, MINOR, PATCH, PRE].compact.join('.')
       end
     end
   end
